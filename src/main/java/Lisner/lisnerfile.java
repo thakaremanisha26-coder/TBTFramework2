40a09e726bbf6a90acc820846ccb1b86a2b0bc11
MANISHA <thakaremanisha26@gmail.com>
2026-04-23 20:34:49 +0530

    TBTFramwork new package add

---
+++ src/main/java/Lisner/lisnerfile.java
@@ -0,0 +1,12 @@
+package Lisner;
+
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
+public class lisnerfile {
+
+	
+	private void syso() {
+		// TODO Auto-generated method stub
+     System.out.println("hellow");
+	}
+}
